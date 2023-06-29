685c2882862ddc8782db7a9b81c97412d15a8762
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-06-29 18:02:42 +0600

    [DONE] JWT and slelect class add to select

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleWire
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorize access' });
+    }
+
+    // brarer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorize access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -27,15 +45,29 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const classesCollection = client.db("heroAcademy").collection("classes");
+        const selectedCollection = client.db("heroAcademy").collection("selected");
+
+
+        // JWT TOKEN
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20h' })
+            res.send({ token });
+        })
 
         // classes collection APIS
         app.get('/classes', async (req, res) => {
-
             const result = await classesCollection.find().toArray();
             res.send(result);
         })
 
-
+        // selected APIS
+        app.post('/selected', async (req, res) => {
+            const classes = req.body;
+            console.log(classes);
+            const result = await selectedCollection.insertOne(classes);
+            res.send(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
